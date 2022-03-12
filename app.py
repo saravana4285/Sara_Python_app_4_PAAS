9dfe2633d10f3140f80b344195567ed3e633419e
Saravanan Muthuramalingam <saravana4285@gmail.com>
2022-03-12 22:50:47 +0530

    Update app.py
    port number is added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #This code is written by Saravanan
+import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def main():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080)
